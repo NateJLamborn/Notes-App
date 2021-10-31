2528ef9ced8537f1685562411c4893045bc0b282
Nate <adfnk>
2021-10-31 13:10:32 -0400

    added delete

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -6,19 +6,15 @@ router.get('/', function(req, res, next) {
     res.send('Note Get');
 });
 
-router.get('/new-note', function(req, res, next){
-    res.render('NewNote', {})
-    res.send('New Note Get')
-});
-
+//create a new note
 router.post('/new-note', function(req, res, next){
     models.notes
       .findOrCreate({
         where: {
-          NoteTitle: req.body.notetitle
+          NoteTitle: req.body.NoteTitle
         },
         defaults: {
-          NoteBody: req.body.notebody,
+          NoteBody: req.body.NoteBody,
         }
       })
       .spread(function (result, created) {
@@ -29,5 +25,47 @@ router.post('/new-note', function(req, res, next){
         }
       });
 })
+
+//get individual note info
+router.get('/:id', function(req, res, next){
+    let noteId = parseInt(req.params.id);
+    models.notes
+    .findOne({
+      where: {
+        NoteId: noteId
+      }
+    })
+    .then(notesFound => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(notesFound));
+    })
+})
+
+//update a note
+router.put('/edit-note/:id', function(req, res, next){
+    let noteId = parseInt(req.params.id);
+    models.notes
+      .update(req.body, { where: { NoteId: noteId } })
+      .then(result => res.send("Note edited"))
+      .catch(err => {
+        res.status(400);
+        res.send("There was a problem editing your note.");
+      });
+  })
+
+//delete a note
+router.delete("/delete-note/:id", function (req, res, next) {
+    let noteId = parseInt(req.params.id);
+    models.notes
+      .destroy({
+        where: { NoteId: noteId }
+      })
+      .then(result => res.send("item deleted"))
+      .catch(err => { 
+        res.status(400); 
+        res.send("There was a problem deleting the item. Please make sure you are specifying the correct title."); 
+      }
+  );
+  });
 
 module.exports = router;
