b6ae9172dc73aa4fb431abb6ada93cac8de0ca58
Nate <adfnk>
2021-11-01 14:32:52 -0400

    fixed form

--- Front End/src/components/AddNote.js
+++ Front End/src/components/AddNote.js
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { 
-    Form, 
-    FormGroup, 
-    Input,
-    Button
-} from 'reactstrap'
 
-export const AddNote = () => {
-    return (
-        <Form>
-            <FormGroup>
-                <Input type="text" placeholder="Enter Title"></Input>
-                <Input type="textarea" placeholder="Enter Note Here"></Input>
-            </FormGroup>
-            <Button type="submit">Submit</Button>
-            <Link to='/' className='btn btn-danger ml-2'>Cancel</Link>
-        </Form>
-    )
+export class AddNote extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             title: "",
+             body: ""
+        }
+    }
+    changeHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+      
+    submitHandler = e => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+
+    
+    render() {
+        const { title, body } = this.state
+        return (
+            <div>
+                <form onSubmit={this.submitHandler}>
+                    <div className='form-group'>
+                        <input type="text" placeholder="Enter Title" name="title" value={title} onChange={this.changeHandler}></input>
+                        <input type="textarea" placeholder="Enter Note Here" name="body" value={body} onChange={this.changeHandler}></input>
+                    </div>
+                    <button type="submit">Submit</button>
+                    <Link to='/' className='btn btn-danger ml-2'>Cancel</Link>
+                    <button type="button" onClick={this.testState}>test</button>
+                </form>
+            </div>
+        )
+    }
 }
